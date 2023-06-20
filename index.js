e1549ea7db39f10c088b0bd6d8d41467b7d0284e
Camille Dev <fezandellecamille@gmail.com>
2023-06-20 14:53:45 +0200

    realization and finalization of project

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+const circles = document.querySelectorAll('.circle');
 
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
-
-// S'assurer que les liens sont clickables
-
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+window.addEventListener('mousemove', (e) => {
+	circles.forEach((circle) => {
+		circle.style.top = e.pageY + 'px';
+		circle.style.left = e.pageX + 'px';
+	});
+});
